9da7af7536b2047f4e7782c050c36306838e8bc3
yulmumu <87139443+yulmumu@users.noreply.github.com>
2023-03-21 23:55:01 +0900

    Add files via upload

---
+++ ArrayEx1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace ArrayEx1 {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aa = new int[100];
+            int[] bb = new int[100];
+            for (int i = 0; i < aa.Length; i++)
+            {
+                aa[i] = (i + 1) * 3;
+
+            }
+            for (int i = 0; i < bb.Length; i++)
+            {
+                int tmp = i - 50;
+                if (tmp < 0)
+                {
+                    tmp += 100;
+                }
+                bb[tmp] = aa[i];
+
+            }
+            Console.WriteLine(bb[0]);
+
+
+            
+        }
+            
+    }
+}
+
+
