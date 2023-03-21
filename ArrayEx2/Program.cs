543d90303e952713aa8a6c910685df54950cbe94
yulmumu <87139443+yulmumu@users.noreply.github.com>
2023-03-22 00:14:08 +0900

    Add files via upload

---
+++ ArrayEx2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ary = new int[3, 4];
+            int value = 1;
+
+            Console.WriteLine(ary.Length);
+            for (int i = 0; i < ary.Length; i++)  // ctrl + shift + /
+            {
+                for (int k = 0; k < ary.Length; k++) 
+                {
+                    ary[i, k] = value;
+                    ++value;
+                }
+
+            }
+
+        }
+    }
+}
